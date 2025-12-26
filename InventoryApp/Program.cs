d29e6a6ce4336dbddebf46ab1af9d66f4f2a367f
Coupsoul <slaw.kapustin2000@gmail.com>
2025-12-26 23:57:19 +0500

    feat(ui): basic menu, shop and inventory status

--- InventoryApp/Program.cs
+++ InventoryApp/Program.cs
@@ -17,62 +17,125 @@ class Program
             Console.Clear();
 
             var service = new InventoryService(context);
-
             var playerName = "Player_01";
 
-
-            Console.WriteLine("----- ИНВЕНТАРЬ -----");
-            var player = await service.GetPlayerAsync(playerName);
-            if (player != null)
+            bool exit = false;
+            while (!exit)
             {
-                Console.WriteLine($"{player.Name}: Gold={player.Gold}, Gems={player.Gems}");
-                Console.WriteLine("Инвентарь:");
+                Console.WriteLine($"Добро пожаловать, {playerName}!");
+
+                Console.WriteLine("\nВыберите действие");
+                Console.WriteLine("1. Мой инвентарь");
+                Console.WriteLine("2. Магазин");
+                Console.WriteLine("3. Обмен валют");
+                Console.WriteLine("0. Выход");
+                Console.Write("> ");
+
+                string? input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        await ShowInventory(service, playerName); break;
+                    case "2":
+                        await Shopping(service, playerName); break;
+                    case "3":
+                        await OpenCurrencyExchanger(service); break;
+                    case "0":
+                        exit = true; break;
+                    default:
+                        Console.WriteLine("Неизвестная команда"); break;
+                }
+            }
+        }
+    }
+
+    static async Task ShowInventory(InventoryService service, string playerName)
+    {
+        Console.WriteLine("----------------  ИНВЕНТАРЬ  ----------------");
+
+        var player = await service.GetPlayerAsync(playerName);
+
+        if (player != null)
+        {
+            Console.WriteLine($"{player.Name}");
+            Console.WriteLine($"Баланс: {player.Gold} золота | {player.Gems} брюлликов");
+            Console.WriteLine("Инвентарь:");
+
+            if (player.Inventory.Count != 0)
+            {
                 foreach (var slot in player.Inventory)
                 {
                     Console.WriteLine($"  [{slot.Amount} шт] {slot.Item.Name}");
                 }
             }
+            else
+                Console.WriteLine("Инвентарь пуст.");
+        }
 
-            Console.WriteLine("\n");
+        Console.WriteLine("Нажмите любую клавишу для продолжения.");
+        WaitAndClear();
+    }
 
-            Console.WriteLine("----------------  СЕМЁРОЧКА  ----------------" + "\nСписок товаров:");
-            var items = await service.GetShopItemsAsync();
-            foreach (var i in items)
+    static async Task Shopping(InventoryService service, string playerName)
+    {
+        var items = await service.GetShopItemsAsync();
+        bool exit = false;
+
+        while (!exit)
+        {
+            var player = await service.GetPlayerAsync(playerName);
+
+            Console.WriteLine("\n----------------  СЕМЁРОЧКА  ----------------");
+            if (player != null)
+                Console.WriteLine($"Баланс: {player.Gold} золота | {player.Gems} брюлликов");
+
+            for (int i = 0; i < items.Count; i++)
             {
-                string currencyName = i.PriceCurrency switch
+                var item = items[i];
+                string currency = item.PriceCurrency switch
                 {
                     Currency.Gold => "золота",
                     Currency.Gems => "брюлликов",
-                    _ => i.PriceCurrency.ToString() // На случай добавления новых типов
+                    _ => item.PriceCurrency.ToString()
                 };
-                var price = i.Price == 0 ? "бесплатно" : $"{i.Price} {currencyName}";
-                Console.WriteLine($" ~ {i.Name} - {price}");
-                if (!string.IsNullOrEmpty(i.Description))
-                    Console.WriteLine($" \"{i.Description}\"\n");
+                string priceStr = item.Price == 0 ? "бесплатно" : $"{item.Price} {currency}";
+
+                Console.WriteLine($"{i + 1}. {item.Name}  -  {priceStr}");
+                if (!string.IsNullOrEmpty(item.Description))
+                    Console.WriteLine($"  \"{item.Description}\"");
             }
 
-            //Console.WriteLine("\n--------  ПРОВЕРКА ПОКУПАТЕЛЬНЫХ ВОЗМОЖНОСТЕЙ  --------");
-            //Console.WriteLine("Берём вилку...");
-            //var result1 = await service.BuyItemAsync(playerName, "Ржавая вилка");
-            //Console.WriteLine(result1);
+            Console.WriteLine("\n~ Введите номер товара для покупки или 0, чтобы выйти:");
+            Console.Write("> ");
+            string? input = Console.ReadLine();
+            if (input == "0")
+            {
+                exit = true;
+                Console.Clear();
+                return;
+            }
+            else if (int.TryParse(input, out int choice) && choice > 0 && choice <= items.Count)
+            {
+                string result = await service.BuyItemAsync(playerName, items[choice - 1].Name);
+                Console.WriteLine(result);
+                Console.WriteLine("Нажмите любую клавишу для продолжения.");
+            }
+            else Console.WriteLine("Некорректный ввод. Нажмите любую клавишу.");
 
-            //Console.WriteLine("Берём зелье...");
-            //var result2 = await service.BuyItemAsync(playerName, "Случайное зелье");
-            //Console.WriteLine(result2);
-
-            //Console.WriteLine("Берём палку...");
-            //var result3 = await service.BuyItemAsync(playerName, "Палка");
-            //Console.WriteLine(result3);
-
-            //Console.WriteLine("Берём вторую вилку...");
-            //var result4 = await service.BuyItemAsync(playerName, "Ржавая вилка");
-            //Console.WriteLine(result4);
-
-            //Console.WriteLine("Пытаемся взять второе зелье...");
-            //var result5 = await service.BuyItemAsync(playerName, "Случайное зелье");
-            //Console.WriteLine(result5);
-
-            Console.ReadKey();
+            WaitAndClear();
         }
+    }
+
+    static async Task OpenCurrencyExchanger(InventoryService service)
+    {
+        Console.WriteLine("Функция в разработке...");
+        WaitAndClear();
+    }
+
+    static void WaitAndClear()
+    {
+        Console.ReadKey(true);
+        Console.Clear();
     }
 }
